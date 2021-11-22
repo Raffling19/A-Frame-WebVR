27ca468392257ab03330a7b75099e2972a01f927
Nico Raffling <nico.raffling@edu.fh-joanneum.at>
2021-11-22 16:42:09 +0100

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,12 +26,6 @@ const config = {
           // Please change this to your repo.
           editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
